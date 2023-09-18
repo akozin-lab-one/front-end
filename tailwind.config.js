3fce2063ebcbd23d06f8a4e14d040617fbb774f1
akozin-lab-one <kozinaung479@gmail.com>
2023-09-18 14:08:28 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        zIndex: {
+            '100': '100',
+        }
+    },
+    plugins: [],
+}
